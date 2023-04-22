82f83a65f9e0bf434fb769236141973dc24b9199
Shane <gupitshanehazel@gmail.com>
2023-04-22 21:09:09 +0800

    make user enter their string

---
+++ multiple commit 1.py
@@ -0,0 +1,13 @@
+# pseudocode
+
+# make user enter their string
+input_str = input("Enter your string: ")
+output_str = ""
+
+# check every character
+    # if *, change a
+    # else, if &, change e
+    # else, if #, change i
+    # else, if +, change o
+    # else, if !, change u
+# print output
