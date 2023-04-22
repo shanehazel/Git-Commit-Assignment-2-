fb4005c365ef29937d2c63ba85b6ae452bfa097d
Shane <gupitshanehazel@gmail.com>
2023-04-22 21:14:43 +0800

    modifying each character and its specific representation

--- multiple commit 1.py
+++ multiple commit 1.py
@@ -6,10 +6,23 @@ output_str = ""
 
 # check every character
 for i in range(len(input_str)):
-    
+
     # if *, change a
+    if input_str[i] == "*":
+        output_str += "a"
     # else, if &, change e
+    elif input_str[i] == "&":
+        output_str +="e"
     # else, if #, change i
+    elif input_str[i] == "#":
+        output_str +="i"
     # else, if +, change o
+    elif input_str[i] == "+":
+        output_str +="o"
     # else, if !, change u
+    elif input_str[i] == "!":
+        output_str +="u"
+    else:
+        output_str += input_str[i]
+        
 # print output
