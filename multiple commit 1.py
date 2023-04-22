5732018ceee79bde79327d609d6854d27ae5d9dd
Shane <gupitshanehazel@gmail.com>
2023-04-22 21:12:01 +0800

    checked every character of the string

--- multiple commit 1.py
+++ multiple commit 1.py
@@ -5,6 +5,8 @@ input_str = input("Enter your string: ")
 output_str = ""
 
 # check every character
+for i in range(len(input_str)):
+    
     # if *, change a
     # else, if &, change e
     # else, if #, change i
